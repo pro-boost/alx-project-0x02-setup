92de59b5db9d0f42df9a54632bc68930878bab3e
MedBrk <150671350+med-ctrl@users.noreply.github.com>
2025-06-02 16:58:30 +0100

    chore(card): Create a Reusable Card Component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,12 @@
+import Card from "@/components/common/Card";
+
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Home Page</h1>
+      <Card title="Card 1" content="Card 1 content" />
+      <Card title="Card 2" content="Card 2 content" />
+      <Card title="Card 3" content="Card 3 content" />
     </div>
   );
 }
